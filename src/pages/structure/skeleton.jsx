605ad4cd27cb0fe6bedbb72dbd944fe6796904f2
Misteranshu <163726815+Misteranshu@users.noreply.github.com>
2025-08-19 02:07:35 +0530

    Updated frontend UI and fixed issues

--- src/pages/structure/skeleton.jsx
+++
@@ -1,9 +0,0 @@
-import { Skeleton } from "@/components/ui/skeleton"
-import {MyLearning} from '@/pages/student/MyLearning'
-
-export function NewSkeleton() {
-  return (
-    <div> <MyLearning/></div>
-   
-  )
-}
